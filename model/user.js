6098fb4481832842f6214ad774e2bebfec9a491a
Riya Jindal <122373500+riyaaaa123@users.noreply.github.com>
2023-03-19 18:05:29 +0530

    Add files via upload

---
+++ model/user.js
@@ -0,0 +1,36 @@
+
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose');
+var User = new Schema({
+	username: {
+		type: String,
+		unique: true, 
+		required:true
+
+
+	},
+
+	name: {
+		type:String,
+		required:true
+
+	},
+	password: {
+		type: String,
+		required:true
+	},
+	cpassword:{
+		type: String,
+		required:true
+	},
+	score:{
+		type:Number,
+		default:0
+	}
+})
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User)
